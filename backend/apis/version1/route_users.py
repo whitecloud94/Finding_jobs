af8ef5c934390127ccdc4ecbce2674d112c4902a
whitecloud94 <ajemfld1@gmail.com>
2021-12-29 12:10:10 +0900

    add ShowUser schemas, add apis/version1/base.py for contain routers

---
+++ backend/apis/version1/route_users.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+from fastapi import Depends
+
+from sqlalchemy.orm import Session
+
+from db.session import get_db
+from db.repository.users import create_new_user
+from schemas.users import UserCreate,ShowUser
+
+router = APIRouter()
+
+@router.post("/", response_model=ShowUser)
+async def create_user(user : UserCreate, db: Session = Depends(get_db)):
+    user = create_new_user(user = user, db=db)
+    return user
--- backend/schemas/users.py
+++ backend/schemas/users.py
@@ -2,8 +2,16 @@ from typing import Optional
 from pydantic import BaseModel, EmailStr
 
 
-#유저 생성하는
+#새로운 유저가 가입할 때 요구받는 인자.
 class UserCreate(BaseModel):
     username: str
     email: EmailStr
     password: str
+
+class ShowUser(BaseModel):
+    username: str
+    email: EmailStr
+    is_active: bool
+
+    class Config():
+        orm_mode = True
