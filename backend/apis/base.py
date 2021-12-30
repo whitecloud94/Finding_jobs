8f1e06bc7fac729adcbbf377fb8859dbb1dc3e24
whitecloud94 <ajemfld1@gmail.com>
2021-12-30 11:52:39 +0900

    fix typo in login module

--- backend/apis/base.py
+++ backend/apis/base.py
@@ -10,4 +10,4 @@ api_router.include_router(route_users.router,prefix="/users",tags=["users"])
 
 api_router.include_router(route_jobs.router, prefix="/jobs", tags=["jobs"])
 
-api_router.include_router(route_login.rouer, prefix="/login", tags=["login"])
+api_router.include_router(route_login.router, prefix="/login", tags=["login"])
